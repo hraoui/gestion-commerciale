b601f49c8db56be6119ca3964f21f90ca1a05d82
hraoui <medhra@gmail.com>
2022-06-24 17:41:10 +0200

    add lfm package

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        Intervention\Image\ImageServiceProvider::class,
     ])->toArray(),
 
 ];
