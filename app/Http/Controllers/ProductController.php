3f18fa44cef00348adbcfe802feceeb2cb3dc882
hraoui <medhra@gmail.com>
2022-06-24 17:46:09 +0200

    add product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::paginate(25);
+
+        return view('inventory.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('inventory.products.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        $request->validate([
+            'name' => 'required',
+            'barcode' => 'required',
+            'description' => 'required',
+            'stock' => 'required|numeric|min:0',
+            'category_id' => 'required',
+            'price' => 'required|numeric|min:0',
+            'photo' => 'required',
+
+        ]);
+        $product = new Product();
+        $product->name = $request->name;
+        $product->barcode = $request->barcode;
+        $product->category_id = $request->category_id;
+        $product->description = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->photo = $request->photo;
+
+        $product->save();
+
+
+        return view('inventory.products', compact('qrcode'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return view('inventory.products.show', ['product' => $product]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        $categories = Category::all();
+        return view('inventory.products.edit', compact('product', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $updateData = $request->validate([
+            'name' => 'required',
+            'barcode' => 'required',
+            'description' => 'required',
+            'stock' => 'required|numeric|min:0',
+            'category_id' => 'required',
+            'price' => 'required|numeric|min:0',
+            'photo' => 'required',
+
+        ]);
+        Product::whereId($id)->update($updateData);
+        return redirect('/products');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return redirect('/products');;
+    }
+
+
+
+}
---
+++ database/migrations/2022_06_24_154216_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('barcode')->nullable();
+            $table->string('photo')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedDecimal('price', 10, 2);
+            $table->unsignedinteger('stock')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
